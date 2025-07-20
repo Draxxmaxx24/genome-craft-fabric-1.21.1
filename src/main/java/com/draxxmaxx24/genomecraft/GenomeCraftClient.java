f9cae8c22b6883a263eae0aaf8e6160683c52507
Draxxmaxx24 <67125992+Draxxmaxx24@users.noreply.github.com>
2025-07-19 23:03:02 -0500

    Initial commit

---
+++ src/main/java/com/draxxmaxx24/genomecraft/GenomeCraftClient.java
@@ -0,0 +1,10 @@
+package com.draxxmaxx24.genomecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GenomeCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
